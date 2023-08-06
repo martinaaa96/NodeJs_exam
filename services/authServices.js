903c8570047a8d018bce2d1bc775af8f0cda5d8a
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-06 16:35:17 +0300

    check exists user

--- services/authServices.js
+++ services/authServices.js
@@ -1,12 +1,21 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
+exports.findByUserName = (username) => User.findOne(username);
+
 
 exports.register = async (username, email, password, repeatPassword) => {
     if (password !== repeatPassword) {
         throw new Error('Password missmatch');
 
     }
+    const existingUser = await this.findByUserName(username);
+    if (existingUser) {
+        throw new Error('User exists');
+
+    }
+
+
     // TODO :Validate password 
 
     const hashedPassword = await bcrypt.hash(password, 10)

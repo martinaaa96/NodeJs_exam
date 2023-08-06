6a44c77be7da82799d305fffc3fe16d3a0b84d90
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-06 16:16:26 +0300

    configure bcrypt and cookir-parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
 
@@ -16,6 +17,7 @@ app.set('view engine', 'hbs');
 app.use('/static', express.static('public'));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(routes);
 
 mongoose.set('strictQuery', false);
--- services/authServices.js
+++ services/authServices.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-
+const bcrypt = require('bcrypt');
 
 
 exports.register = async (username, email, password, repeatPassword) => {

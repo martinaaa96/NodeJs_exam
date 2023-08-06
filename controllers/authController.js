3f340d2571aadb9d492d5807a5ccef7c5ff7f375
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-06 16:43:09 +0300

    check if password is valid

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,9 +16,10 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/login', (req, res) => {
+router.post('/login',async (req, res) => {
     const { email, password } = req.body;
 
+ await authService.login(email,password)
 });
 
 
--- services/authServices.js
+++ services/authServices.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 
 exports.findByUserName = (username) => User.findOne(username);
 
+exports.findByEmail = (email) => User.findOne(email);
 
 exports.register = async (username, email, password, repeatPassword) => {
     if (password !== repeatPassword) {
@@ -22,4 +23,19 @@ exports.register = async (username, email, password, repeatPassword) => {
     await User.create({ username, email, password: hashedPassword });
 
 
+}
+
+
+exports.login = async (email, password) => {
+    const user = await this.findByEmail(email);
+
+    if(!user){
+       throw new Error ('Invalid email or password');
+
+    }
+   const isValid = await  bcrypt.compare(user.password,password);
+
+   if(!isValid){
+    throw new Error ('Invalid email or password');
+   }
 }

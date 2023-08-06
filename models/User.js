c644e848990c8d0d26350c2b23f64ad328800c63
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-06 15:56:50 +0300

    Create User Model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+    
+},{
+virtuals: {
+    repeatPassword:{
+        set(value){
+           if(this.password !== value){
+            throw new mongoose.Error('Password missmatch');
+
+           }
+        }
+    }
+}
+}
+);
+
+const User = mongoose.model('User',userSchema);

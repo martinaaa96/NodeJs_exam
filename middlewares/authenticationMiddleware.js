9d834cefd5be91162cc1b96c54413d90d6d2a74c
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-06 18:00:33 +0300

    add authentication moddleware

---
+++ middlewares/authenticationMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('../lib/jsonwebtoken');
+
+
+exports.authentication = async (req, res, next) => {
+
+    const token = req.cookies['auth'];
+
+    if (token) {
+        try {
+            const decodedToken = await jwt.verify(token);
+        } catch (error) {
+            
+            res.clearCookie('auth');
+            return res.status(401).render('home/404');
+
+        }
+
+
+    }
+
+    next();
+
+}

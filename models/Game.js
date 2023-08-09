c40957737193e5ba1844818bc82666b16370bd39
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-09 10:32:19 +0300

    create product Schema

---
+++ models/Game.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    platform: {
+        type: String,
+        enum: ['PC', 'Nintendo', 'PS4', 'PS5', 'XBOX'],
+        required: true
+    },
+    boughtBy: {
+        type: [],
+        required: true
+    },
+    owner: {
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+
+    },
+
+})

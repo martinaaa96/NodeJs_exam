735db95e66f976db8baf282f55044fb8098d6eb9
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-09 12:07:06 +0300

    show static ditails page

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -5,13 +5,17 @@ const gameService = require('../services/gameServices');
 const { getErrorMessage } = require('../utils/utils');
 
 
-router.get('/catalog',async (req,res)=>{
+router.get('/catalog', async (req, res) => {
 
-   const game =  await  gameService.getAll();
+    const game = await gameService.getAll();
 
-    res.render('game/catalog', {game });
-    
+    res.render('game/catalog', { game });
+
 });
+
+router.get('/:gameId/details', (req, res) => {
+res.render('game/details');
+})
 
 router.get('/create', isAuth, (req, res) => {
     res.render('game/create');
@@ -22,7 +26,7 @@ router.post('/create', isAuth, async (req, res) => {
     const gameData = req.body;
     try {
         await gameService.create(req.user._id, gameData);
-       
+
 
     } catch (error) {
         return res.status(400).render('game/create', { error: getErrorMessage(error) })
